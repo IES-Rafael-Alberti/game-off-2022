aad26824d24bb2645c6850c90cf76b8ca88ffab3
Aletuno <alejandroaceytunogonzalez@gmail.com>
2022-11-24 12:03:21 +0100

    hotfix

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,9 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public LoadManager loadManager;
     public string sceneName = "";
     public Vector3 newRespawn = Vector3.zero;
+
+    private LoadManager loadManager;
+
+    private void Start()
+    {
+        loadManager = GameObject.Find("Load Manager").GetComponent<LoadManager>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player")) {
