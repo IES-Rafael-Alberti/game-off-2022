8f7fd2efda14ec9507b5ef127afee53d97d160cd
Aletuno <alejandroaceytunogonzalez@gmail.com>
2022-11-26 12:19:22 +0100

    Mushroom Tripiing

--- Assets/Scripts/Events/MushroomEvent.cs
+++ Assets/Scripts/Events/MushroomEvent.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class MushroomEvent : DefaultEvent
 {
     public GameObject canvasEffect;
+    public float trippingTime = 3f;
+
+    private SpriteRenderer mushroom;
+    private PopUpEnter popUpComponent;
+    private bool isTripping;
+
+    private void Start()
+    {
+        mushroom = transform.parent.gameObject.GetComponent<SpriteRenderer>();
+        popUpComponent = GetComponent<PopUpEnter>();
+    }
     public override void EventOnStay(PlayerMovement player)
     {
-        gameObject.SetActive(false);
-        StartCoroutine(Tripping());
+        if (!isTripping) StartCoroutine(Tripping());
     }
     IEnumerator Tripping()
     {
-        Debug.Log("hola");
-        yield return null;
+        isTripping = true;
+        mushroom.enabled = false;
+        popUpComponent.enabled = false;
+        popUpComponent.popUp.enabled = false;
+        canvasEffect.SetActive(true);
+        yield return new WaitForSeconds(trippingTime);
+        canvasEffect.SetActive(false);
+        transform.parent.gameObject.SetActive(false);
     }
 }
