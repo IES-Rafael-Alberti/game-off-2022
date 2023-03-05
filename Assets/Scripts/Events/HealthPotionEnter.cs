873953bbc07dde9bd816e3be26334c5dbe83da00
fjortegan <fjortegan@gmail.com>
2023-03-05 20:48:50 +0100

    tooltip init

--- Assets/Scripts/Events/HealthPotionEnter.cs
+++ Assets/Scripts/Events/HealthPotionEnter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthPotionEnter : DefaultEvent
+public class HealthPotionEnter : DefaultEvent, ITextInfo
 {
 
     public float healAmount = 1;
@@ -11,5 +11,15 @@ public class HealthPotionEnter : DefaultEvent
     {
         player.gameObject.GetComponent<PlayerHealth>().RecoverHealth(healAmount);
         gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        Debug.Log(this);
+    }
+
+    public override string ToString()
+    {
+        return $"Cura {healAmount} vidas.";
     }
 }
